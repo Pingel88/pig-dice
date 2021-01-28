26d9b93c750417eb720124c1f0c156724c5d86e8
dcouch440 <dcouch440@gmail.com>
2021-01-28 10:55:16 -0800

    add experiment
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
+function Game() {
+  this.turn = undefined;
+  this.currentAction = undefined;
+}
+Game.prototype.setTurn = function(arg){
+  this.turn = arg
+}
+Game.prototype.changeAction = function(arg) {
+  this.currentAction = arg
+}
 function Player(name) {
   this.name = name;
   this.score = 0;
@@ -18,62 +28,60 @@ Player.prototype.removePoints = function() {
 Player.prototype.addTotalPoints = function() {
   this.totalScore = this.score + this.totalScore
 }
-let currentAction
-let turn;
-
-function pig(P1, P2, functionArg = currentAction) {
+function pig(P1, P2, game) {
+  console.log(game)
   let nextTurn = 'current-turn';
-  if (turn === undefined){
+  if (game.turn === undefined){
     const firstInt = getRandomInt(100);
     if (firstInt > 50) {
-      turn = P1.name
+      game.setTurn(P1.name)
     }
     else {
-      turn = P2.name
+      game.setTurn(P2.name)
     }
   }
-  if (functionArg === 'turn') {
+  if (game.currentAction === 'turn') {
     nextTurn = 'next-turn';
     roll()
-  } else if (functionArg === 'roll') {
+  } else if (game.currentAction === 'roll') {
     roll()
   }
   function roll() {
     let randomInt = getRandomInt(6);
     // IF ONE YOU LOSE POINTS
     if (nextTurn === 'next-turn'){
-      if (turn === P1.name) {
+      if (game.turn === P1.name) {
         P1.addTotalPoints()
         P1.removePoints()
-        turn = P2.name
+        game.setTurn(P2.name)
         nextTurn = 'current-turn'
       } else if (turn === P2.name) {
         P2.addTotalPoints()
         P2.removePoints()
-        turn = P1.name
+        game.setTurn(P1.name)
         nextTurn = 'current-turn'
       };
     } else if (randomInt === 1) {
-      if (turn === P1.name) {
+      if (game.turn === P1.name) {
         P1.removePoints()
-        turn = P2.name
+        game.setTurn(P2.name)
         nextTurn = 'current-turn'
-      } else if (turn === P2.name) {
+      } else if (game.turn === P2.name) {
         P2.removePoints()
-        turn = P1.name
+        game.setTurn(P1.name)
         nextTurn = 'current-turn'
       };
       // IF NEXT TURN ADD SCORE TO TOTAL POINTS
       // IF NOT NEXT TURN OR 1 ACCUMULATE
     } else {
-      if (turn === P1.name) {
+      if (game.turn === P1.name) {
         if (P1.score + randomInt + P1.totalScore >= 100) {
           displayWinner(P1.name);
           console.log(`${P1.name} WINS!`)
         } else {
           P1.addPoints(randomInt)
         }
-      } else if (turn === P2.name) {
+      } else if (game.turn === P2.name) {
         if (P2.score + randomInt + P2.totalScore >= 100) {
           displayWinner(P2.name);
           console.log(`${P2.name} WINS!`)
@@ -96,6 +104,7 @@ function diceRoll(roll){
 }
 
 $(document).ready(function() {
+  let pigGame = new Game()
   let playerOne;
   let playerTwo;
 
@@ -112,25 +121,23 @@ $(document).ready(function() {
     console.log(playerTwo);
     $("#player-two").hide();
     $("#game").show();
-    pig(playerOne, playerTwo);
+    pig(playerOne, playerTwo, pigGame);
   });
   $('#next-turn').click(function(event) {
     event.preventDefault()
-    currentAction = 'turn'
-    pig(playerOne, playerTwo)
-    console.log('Next Turn')
-    console.log("\n")
+    pig(playerOne, playerTwo, pigGame)
+    // console.log('Next Turn')
+    // console.log("\n")
   })
   $('#roll').click(function(event) {
     event.preventDefault()
-    currentAction = 'roll'
-    pig(playerOne, playerTwo)
-    console.log('New Roll')
-    console.log(playerOne.name + " score: " + playerOne.score)
-    console.log(playerOne.name + " score: " + playerOne.totalScore)
-    console.log("\n")
-    console.log(playerTwo.name + " score: " + playerTwo.score)
-    console.log(playerTwo.name + " score: " + playerTwo.totalScore)
-    console.log("\n")
+    pig(playerOne, playerTwo, pigGame)
+    // console.log('New Roll')
+    // console.log(playerOne.name + " score: " + playerOne.score)
+    // console.log(playerOne.name + " score: " + playerOne.totalScore)
+    // console.log("\n")
+    // console.log(playerTwo.name + " score: " + playerTwo.score)
+    // console.log(playerTwo.name + " score: " + playerTwo.totalScore)
+    // console.log("\n")
   })
 });
